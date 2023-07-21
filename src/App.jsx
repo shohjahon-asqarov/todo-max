c2c4c261cc5fb745989858b0922cbe1f91097cc6
Shohjahon <104879567+mr-webprogrammer@users.noreply.github.com>
2023-07-21 14:08:29 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Main from './components/Main';
+
+import './index.css';
+
+function App() {
+
+  return (
+    <div className='container min-h-screen'>
+      <Main />
+    </div>
+  )
+}
+
+export default App
